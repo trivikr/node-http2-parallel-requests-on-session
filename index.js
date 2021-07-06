c533ce50d4effc0917aece4f46ab3a979a1e3c02
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-06 16:59:09 -0700

    chore: send stream to server and return data at random intervals

--- index.js
+++ index.js
@@ -3,28 +3,51 @@ const assert = require("assert");
 
 (() => {
   const server = http2.createServer();
-  server.on("stream", (stream, headers) => {
-    stream.respond({
-      "content-type": "text/html",
-      ":status": 200,
-    });
-    stream.end("<h1>Hello World</h1>");
-  });
-  server.listen(8000);
 
-  const client = http2.connect("http://localhost:8000");
-  const req = client.request();
-  req.on("response", (headers) => {
-    assert.strictEqual(headers[":status"], 200);
-    assert.strictEqual(headers["content-type"], "text/html");
+  server.on("stream", (stream, headers) => {
+    stream.setEncoding("utf8");
+    stream.on("data", (data) => {
+      setTimeout(() => {
+        stream.write(data);
+      }, Math.random() * 1000);
+    });
+    stream.on("end", () => {
+      // End the stream after one second.
+      setTimeout(() => {
+        stream.end();
+      }, 1000);
+    });
   });
-  req.setEncoding("utf8");
-  req.on("data", (chunk) => {
-    assert.strictEqual(chunk, "<h1>Hello World</h1>");
+
+  server.listen(8000, () => {
+    const client = http2.connect("http://localhost:8000");
+    const req = client.request({ ":method": "POST" });
+
+    const totalLength = 1000;
+    let remaining = totalLength;
+    const chunkLength = 10;
+    const charToRepeat = "a";
+    const chunk = Buffer.alloc(chunkLength, charToRepeat);
+
+    const writeChunk = () => {
+      if (remaining > 0) {
+        remaining -= chunkLength;
+        req.write(chunk, writeChunk);
+      } else {
+        req.end();
+      }
+    };
+    writeChunk();
+
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("close", () => {
+      assert.strictEqual(data, charToRepeat.repeat(totalLength));
+      client.close();
+      server.close();
+    });
+    req.resume();
   });
-  req.on("end", () => {
-    client.destroy();
-    server.close();
-  });
-  req.end();
 })();
