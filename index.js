6bfa690c04be1eebcebc71115b3c152c4f68c178
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-06 17:40:41 -0700

    feat: use yargs to process command line arguments

--- index.js
+++ index.js
@@ -1,66 +1,80 @@
 const http2 = require("http2");
 const assert = require("assert");
 
-(() => {
-  const options =
-    process.argv.length === 3 && process.argv[2] === "limit-concurrent-streams"
-      ? { settings: { maxConcurrentStreams: 1 } }
-      : {};
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 
-  const server = http2.createServer(options);
+const argv = yargs(hideBin(process.argv))
+  .usage("Usage:\n" + "  test\n" + "    [--max-concurrent-streams <number>]\n")
+  .options({
+    maxConcurrentStreams: {
+      type: "number",
+      description:
+        "Specifies the maximum number of concurrent streams permitted on an Http2Session." +
+        "There is no default value which implies, at least theoretically, 2^32-1 streams " +
+        "may be open concurrently at any given time in an Http2Session. The minimum value is 0." +
+        " The maximum allowed value is 2^32-1. Default: 4294967295.",
+    },
+  })
+  .help().argv;
 
-  server.on("stream", (stream, headers) => {
-    stream.setEncoding("utf8");
-    stream.on("data", (data) => {
-      setTimeout(() => {
-        stream.write(data);
-      }, Math.random() * 1000);
-    });
-    stream.on("end", () => {
-      // End the stream after one second.
-      setTimeout(() => {
-        stream.end();
-      }, 1000);
-    });
-  });
+const options = argv.maxConcurrentStreams
+  ? { settings: { maxConcurrentStreams: argv.maxConcurrentStreams } }
+  : {};
 
-  server.listen(8000, () => {
-    const client = http2.connect("http://localhost:8000");
+const h2Server = http2.createServer(options);
 
-    const testRequest = (charToRepeat) => {
-      const req = client.request({ ":method": "POST" });
-      const totalLength = 1000;
-      let remaining = totalLength;
-      const chunkLength = 10;
-      const chunk = Buffer.alloc(chunkLength, charToRepeat);
-
-      const writeChunk = () => {
-        if (remaining > 0) {
-          remaining -= chunkLength;
-          req.write(chunk, writeChunk);
-        } else {
-          req.end();
-        }
-      };
-      writeChunk();
-
-      let data = "";
-      req.on("data", (chunk) => {
-        data += chunk;
-      });
-      req.on("close", () => {
-        assert.strictEqual(data, charToRepeat.repeat(totalLength));
-      });
-      req.resume();
-    };
-
-    testRequest("a");
-    testRequest("b");
-
-    // Close client and server after two seconds.
+h2Server.on("stream", (stream, headers) => {
+  stream.setEncoding("utf8");
+  stream.on("data", (data) => {
     setTimeout(() => {
-      client.close();
-      server.close();
-    }, 2000);
+      stream.write(data);
+    }, Math.random() * 1000);
   });
-})();
+  stream.on("end", () => {
+    // End the stream after one second.
+    setTimeout(() => {
+      stream.end();
+    }, 1000);
+  });
+});
+
+h2Server.listen(8000, () => {
+  const clientH2Session = http2.connect("http://localhost:8000");
+
+  const testRequest = (charToRepeat) => {
+    const req = clientH2Session.request({ ":method": "POST" });
+    const totalLength = 1000;
+    let remaining = totalLength;
+    const chunkLength = 10;
+    const chunk = Buffer.alloc(chunkLength, charToRepeat);
+
+    const writeChunk = () => {
+      if (remaining > 0) {
+        remaining -= chunkLength;
+        req.write(chunk, writeChunk);
+      } else {
+        req.end();
+      }
+    };
+    writeChunk();
+
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("close", () => {
+      assert.strictEqual(data, charToRepeat.repeat(totalLength));
+    });
+    req.resume();
+  };
+
+  testRequest("a");
+  testRequest("b");
+
+  // Close client and server after two seconds.
+  setTimeout(() => {
+    clientH2Session.close();
+    h2Server.close();
+  }, 2000);
+});
